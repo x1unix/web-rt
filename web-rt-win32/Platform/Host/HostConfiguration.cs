fc3e3b3ba01f7f153d84d2b39e28a3f389d5606a
odin3 <ascii@live.ru>
2017-08-27 03:18:35 +0300

    Add manifest and configuration interfaces

---
+++ web-rt-win32/Platform/Host/HostConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebRT.Platform.Host
+{
+    class HostConfiguration
+    {
+        /// <summary>
+        /// Location of globally installed packages
+        /// </summary>
+        public string GlobalPackagesLocation { get; set; }
+        public string LocalPackagesLocation { get; set; }
+        public bool DebugModeEnabled { get; set; }
+
+        public static readonly string LocalRootDirectoryName = ".webrt";
+        public static readonly string PackagesDirectoryName = "Applications";
+
+        public HostConfiguration()
+        {
+            this.GlobalPackagesLocation = FormatPath(AppDomain.CurrentDomain.BaseDirectory, PackagesDirectoryName);
+            this.LocalPackagesLocation = FormatPath(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), PackagesDirectoryName);
+        }
+
+        /// <summary>
+        /// Get location of WebRT folder for specific directory root
+        /// </summary>
+        /// <param name="root">Root directory</param>
+        /// <param name="path">Path</param>
+        /// <returns></returns>
+        private static string FormatPath(string root, string path)
+        {
+            return $"{root}\\{LocalRootDirectoryName}\\${path}";
+        }
+    }
+}
---
+++ web-rt-win32/Platform/Host/HostConfigurationProvider.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebRT.Platform.Host
+{
+    /// <summary>
+    /// Provides runtime host configuration
+    /// </summary>
+    class HostConfigurationProvider
+    {
+        /// <summary>
+        /// Saved instance
+        /// </summary>
+        private static HostConfigurationProvider Instance;
+
+        /// <summary>
+        /// Get provider's instance
+        /// </summary>
+        /// <returns></returns>
+        private static HostConfigurationProvider GetInstance()
+        {
+            if (Instance == null)
+            {
+                Instance = new HostConfigurationProvider();
+            }
+
+            return Instance;
+        }
+
+        /// <summary>
+        /// Saved configuration instance
+        /// </summary>
+        private HostConfiguration Configuration;
+
+        /// <summary>
+        /// Get host configuration
+        /// </summary>
+        /// <returns></returns>
+        public HostConfiguration GetConfiguration()
+        {
+            if (Configuration == null)
+            {
+                Configuration = new HostConfiguration();
+            }
+
+            return Configuration;
+        }
+    }
+}
